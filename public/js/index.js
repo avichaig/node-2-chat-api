305bad90f1c514eca3ed5cab640442aef0cac831
Avichai  Gez <avichaigez@gmail.com>
2018-12-19 10:33:01 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to server');
-    
-    socket.emit('createMessage', {
-        from: 'avigii',
-        text: 'hi, how u doing (;'
-    });
 });
 
 socket.on('disconnect', () => {
